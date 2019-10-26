f9ef447d64c6d94127e1a9915dad102ce4dbed88
Maensh <is4syria@gmail.com>
2019-10-26 09:55:52 +0300

    Scope class demonstrates field and local variable scopes.

--- TestJava/src/testjava/TestJava.java
+++ TestJava/src/testjava/TestJava.java
@@ -63,6 +63,10 @@ public class TestJava {
     /**
      * @param args the command line arguments
      */
+    
+    // field that is accessible to all methods of this class
+    //static int x = 1;
+    
     public static void main(String[] args) {
         // TODO code application logic here
         /*Scanner input = new Scanner(System.in);
@@ -112,6 +116,33 @@ public class TestJava {
                 System.out.println();
         }*/
         
+        /*int x = 5; // method's local variable x shadows field x
+
+        System.out.printf("local x in main is %d%n", x);
+
+        useLocalVariable(); // useLocalVariable has local x
+        useField(); // useField uses class Scope's field x
+
+        System.out.printf("%nlocal x in main is %d%n", x);*/
+
+    }
+    
+    // create and initialize local variable x during each call
+    public static void useLocalVariable() {
+        int x = 25; // initialized each time useLocalVariable is called
+
+        System.out.printf("%nlocal x on entering method useLocalVariable is %d%n",
+            x);
+        ++x; // modifies this method's local variable x
+        System.out.printf("local x before exiting method useLocalVariable is %d%n",
+            x);
+    }
+
+    // modify class Scope's field x during each call
+    public static void useField() {
+        System.out.printf("%nfield x on entering method useField is %d%n", x);
+        x *= 10; // modifies class Scope's field x
+        System.out.printf("field x before exiting method useField is %d%n", x);
     }
     
 }
